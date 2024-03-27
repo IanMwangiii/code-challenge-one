8adaf2a4728b9b366e8c48c7fbd895729a6b10de
Ian <ian.mwangi1@student.moringaschool.com>
2024-03-27 09:22:45 +0300

    Code challenge 1

--- Grade.js
+++ Grade.js
@@ -1,18 +1,17 @@
-function calculateGrade(marks) {
-    //lets create an if statement to check thtough
-    if (marks > 79) {
-        return "A";
-    } else if (marks >= 60 && marks <= 79) {
-        return "B";
-    } else if (marks >= 50 && marks <= 59) {
-        return "C";
-    } else if (marks >= 40 && marks <= 49) {
-        return "D";
+// Prompts user to enter marks
+const marks = prompt("Enter marks:");
+let grade;
+// if fetchs users input and assigns it an appropriate grade
+    if(marks >= 79) {
+        grade = "A";
+    } else if (marks > 59 && marks < 79 ) {
+        grade = "B";
+    } else if (marks >= 49 && marks <= 59) {
+        grade = "C";
+    } else if (marks >= 40 && marks < 49) {
+        grade = "D";
     } else {
-        return "E";
+        grade = "E";
     }
-}
-//prompt user for input
-let marks = prompt("please enter your marks: ")
-console.log(marks)
-console.log("Your grade is:", calculateGrade(marks));
+    // returns grade
+    console.log(grade)
